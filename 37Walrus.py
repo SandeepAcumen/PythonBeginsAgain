55f659c290dec0264f9e46e7169d4ed9053ec521
SandeepAcumen <sandeepkumarg.acumenusa@gmail.com>
2025-09-05 17:03:34 +0530

    Day 7

---
+++ 37Walrus.py
@@ -0,0 +1,22 @@
+def very_slow_function():
+    print("Something......")
+    print("Something......")
+    print("Something......")
+    print("Something......")
+    print("Something......")
+    print("Something......")
+    return 55
+
+# a= very_slow_function()
+# if(a>10):
+#     print(a)
+
+# else:
+#     print("its not a grater than 10")
+
+while(data:=input("Enter the value :")):
+    print(data)
+    if data =="q":
+        break
+
+    
