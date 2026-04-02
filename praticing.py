4ef70bfebb27a1bde453a5bd0eac810e0993d9eb
SandeepAcumen <sandeepkumarg.acumenusa@gmail.com>
2026-04-03 00:31:52 +0530

    Databricks QA

---
+++ praticing.py
@@ -0,0 +1,57 @@
+'''
+
+str1 = 'My'
+str2 = 'Name'
+str3 = 'Is'
+str4 = 'James'
+
+print(str1,str2,str3,str4,sep="**")
+print(str1,str2,str3,str4,sep=" ")
+
+'''
+
+'''
+num = 458.541315
+print('%.2f' % num)'''
+
+
+'''
+str1 ,str2 ,str3 = input("Enter name:").split()
+print(str1)
+print(str2)
+print(str3)
+
+
+'''
+'''
+with open(test.txt) as fp:
+    lines =fp.readlines()
+
+    print(lines[2])'''
+
+'''
+i =1
+while i<11:
+    print(i)
+    i =i+1
+
+'''
+
+'''
+n =3
+for i in range(1,11):
+    product = n*i
+    print(product)
+
+    '''
+'''
+numbers = [12, 75, 150, 180, 145, 525, 50]
+
+for n in numbers:
+    print(n)
+    if n ==180:
+        break
+        
+'''
+
+n= 12344
